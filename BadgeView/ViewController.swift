e42f413017bb9eb490b7fd88220f4c6613c43248
Shrawan Shinde <shrawanshinde@Shrawans-MacBook-Pro.local>
2017-01-14 01:04:41 +0530

    The value of this property is the integer value displayed in the app’s badge at the time the push notification was sent. Edit

--- BadgeView/ViewController.swift
+++ BadgeView/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let badgeCount: Int = 10
+        let application = UIApplication.shared
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options:[.badge, .alert, .sound]) { (granted, error) in
+            // Enable or disable features based on authorization.
+            //Disable this line to display notification count on app
+            UIApplication.shared.applicationIconBadgeNumber = 0;
+        }
+        application.registerForRemoteNotifications()
+        application.applicationIconBadgeNumber = badgeCount
     }
 
     override func didReceiveMemoryWarning() {
